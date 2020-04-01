b2f720eecc1e8ee626f79583c22702ab2f64ae32
apang <1305999617@qq.com>
2020-04-01 18:35:57 +0800

    '静态资源'

--- day1/watchlist/app.py
+++ day1/watchlist/app.py
@@ -1,8 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 
 def index():
 
-    return '<h1>Hello, Falsk</h1>'
+    name = 'Apang'
+
+    movies = [
+        {"title":"大赢家","year":"2020"},
+        {"title":"囧妈","year":"2020"},
+        {"title":"战狼","year":"2020"},
+        {"title":"心花路放","year":"2018"},
+        {"title":"速度与激情8","year":"2012"},
+        {"title":"我的父亲母亲","year":"1995"},
+        {"title":"囧妈","year":"2020"},
+        {"title":"战狼","year":"2020"},
+        {"title":"心花路放","year":"2018"},
+        {"title":"囧妈","year":"2020"},
+        {"title":"战狼","year":"2020"},
+        {"title":"心花路放","year":"2018"}
+    ]
+
+    return render_template('index.html', name=name, movies=movies)
